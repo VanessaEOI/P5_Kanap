cd05bdafd2e1d5f8e53cdf572612f4a4a5dd4209
Vanessa Estival <vanessaestival@gmail.com>
2022-03-31 17:38:50 +0200

    modif index js without innerHTML

--- front/js/script.js
+++ front/js/script.js
@@ -1,33 +1,49 @@
-/* */
+//Get available products
+getProducts();
 
-class Product{
-    constructor(jsonProduct){
-        jsonProduct && Object.assign(this, jsonProduct);
-    }
+
+//Create products from above list
+createProducts();
+
+async function getProducts() {
+    let products = await fetch('http://localhost:3000/api/products');
+    console.log("Products successfully retrieved !")
+    return products.json();
 }
 
-/* */
+async function createProducts() {
+    let result = await getProducts()
+    .then( (product) => {
+        for (let i=0; i < product.length; i++) {		
 
-class ProductManager{
-    constructor(listProduct){
-        this.listProduct = listProduct;
-    }
-}
+            // Insert <a>
+            let productLink = document.createElement("a");
+            document.querySelector(".items").appendChild(productLink);
+            productLink.href = `product.html?id=${product[i]._id}`;
 
-/* */
+            // Insert <article>
+            let productArticle = document.createElement("article");
+            productLink.appendChild(productArticle);
 
-fetch("http://localhost:3000/api/products")
-    .then (data => data.json()) /* Transform txt to json */
-    .then (jsonListProduct => {
-        for(let jsonProduct of jsonListProduct){
-            let product = new Product(jsonProduct);
-            document.getElementById("items").innerHTML += 
-            `<a href="./product.html?${product._id}">
-                <article>
-                    <img src="${product.imageUrl}" alt="${product.altTxt}"/>
-                    <h3 class="productName">${product.name}</h3>
-                    <p class="productDescription">${product.description}</p>
-                </article>
-            </a>`
+            // Insert <img>
+            let productImg = document.createElement("img");
+            productArticle.appendChild(productImg);
+            productImg.src = product[i].imageUrl;
+            productImg.alt = product[i].altTxt;
+
+            // Insert <h3>
+            let productName = document.createElement("h3");
+            productArticle.appendChild(productName);
+            productName.classList.add("productName");
+            productName.innerHTML = product[i].name;
+
+            // Insert <p>
+            let productDescription = document.createElement("p");
+            productArticle.appendChild(productDescription);
+            productDescription.classList.add("productName");
+            productDescription.innerHTML = product[i].description;
         }
     });
+    console.log("Products have been created !");
+
+}
